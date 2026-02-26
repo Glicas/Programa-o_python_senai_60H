d3418f30820355a4eaa377e19987649d652e7315
Miguel <miguelmenezescs@gmail.com>
2026-02-26 17:39:21 -0300

    Add files via upload

---
+++ python programação idle.py
@@ -0,0 +1,10 @@
+#juntar dados:
+
+print("ola mundo","nome")
+print("resultado da soma = ", 2+2)
+print("resultado da dviisão : ", 2 // 2)
+print("altura : " ,1.71)
+print("R$", 500.0)
+
+name
+print('teste','name', 2, 5.2, false)
