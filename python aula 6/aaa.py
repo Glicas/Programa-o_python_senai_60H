42404b0729c343c5755bdcee51b0fe18f903b5e1
Miguel <miguelmenezescs@gmail.com>
2026-03-05 15:40:10 -0300

    Add files via upload

---
+++ python aula 6/aaa.py
@@ -0,0 +1,143 @@
+print("EXERCÍCIO 1")
+
+resultado = (4 * 6) / 2
+print('Resultado',resultado)
+
+print("------------------------------------------------")
+print("")
+
+print("EXERCÍCIO 2")
+
+ola = 5 + 7
+print('soma',ola)
+
+print("------------------------------------------------")
+print("")
+
+print("EXERCÍCIO 3")
+
+potencia = 3 ** 4
+print('potência', potencia)
+
+print("------------------------------------------------")
+print("")
+
+print("EXERCÍCIO 4")
+
+media = (8 + 12 + 15) / 3
+
+print('média',media)
+
+
+print("------------------------------------------------")
+print("")
+
+print("EXERCÍCIO 5")
+
+seila = (10 - 2) * 5
+print("subtração com multiplicação e o seu resultado", seila)
+
+print("------------------------------------------------")
+print("")
+
+print("EXERCÍCIO 6")
+
+divisao = (27 / 3) + 5
+print('divisão + soma', divisao)
+
+print("------------------------------------------------")
+print("")
+
+print("EXERCÍCIO 7")
+
+olaaaa = 17 % 4
+print(olaaaa)
+
+print("------------------------------------------------")
+print("")
+
+print("EXERCÍCIO 8")
+
+quadrado = float(input('digite o quadrado'))
+
+soma = (9*3) ** quadrado
+
+print('resultado', soma)
+
+
+print("------------------------------------------------")
+print("")
+
+print("EXERCÍCIO 9")
+
+somas = 81 ** 0.5
+
+print('resultado',somas)
+
+print("------------------------------------------------")
+print("")
+
+print("EXERCÍCIO 10")
+
+numerosss = 20
+
+p = 20 + (3 * 4)
+
+print('resultado',p)
+
+print("------------------------------------------------")
+print("")
+
+print("EXERCÍCIO 11")
+
+L = (15 * 2) - 7
+print('resultado', L)
+
+print("------------------------------------------------")
+print("")
+
+print("EXERCÍCIO 12")
+
+cubo = float(input('digite o cubo'))
+
+cubo2 = 5 ** cubo
+
+print("resultado",cubo2)
+
+print("------------------------------------------------")
+print("")
+
+print("EXERCÍCIO 13 ")
+
+media2 = (17 + 21 + 25) / 3
+
+print('resultado', media2)
+
+print("------------------------------------------------")
+print("")
+
+print("EXERCÍCIO 14 ")
+
+U = (11 * 2) + 7
+
+print("resultado", U)
+
+
+print("------------------------------------------------")
+print("")
+
+print("EXERCÍCIO 15 ")
+
+P = ((3 * 8) - 15) / 2
+
+print(' resultado',  P)
+
+print("------------------------------------------------")
+print("")
+
+print("EXERCÍCIO 16 ")
+
+M = 2 ** 10
+
+print('resultado', M)
+
