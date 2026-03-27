48c07cb2bfe33479c9207be88fc80f070daa190a
Miguel <miguelmenezescs@gmail.com>
2026-03-27 17:03:34 -0300

    Add files via upload

---
+++ aula 15/test_minhas_funcoes.py
@@ -0,0 +1,9 @@
+import funcoes
+
+def teste_funcao():
+    assert funcoes.soma(2,3,1) == 6
+    assert funcoes.soma(5,5,5) == 15
+    assert funcoes.sub(100,2) == 98
+    assert funcoes.sub(8,4) == 4
+
+    
