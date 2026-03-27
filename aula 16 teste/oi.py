be76c4b224e3ec5522192ca7f3c0e4c6c5c212ea
Miguel <miguelmenezescs@gmail.com>
2026-03-27 17:56:34 -0300

    Add files via upload

---
+++ aula 16 teste/oi.py
@@ -0,0 +1,19 @@
+import pygame
+
+pygame.init()
+
+
+tela = pygame.display.set_mode((500,500))
+run = True
+
+
+while run:
+   for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+         run = False
+         pygame.quit()
+     
+   tela.fill((55,25,120))
+   pygame.draw.rect(tela, ('#ffffff'), (235,235,30,30))
+   pygame.display.update()
+pygame.quit
